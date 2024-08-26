534e5f6a5ed68003b737a45cc919f06b587b29b2
LH <benedictwanlihau@hotmail.com>
2024-08-26 21:33:48 +0800

    update - updating app to scrollTop on route change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -13,6 +13,8 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([requestInterceptor, responseInterceptor])
     ),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: "top",
+    })),
     provideAnimationsAsync()]
 };
